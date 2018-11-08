dcd8018b8af71b9a307c87e342bb852623a7a93c
Novikov <novikovmailg@gmail.com>
2018-11-08 10:31:41 +0200

    Add project files.

---
+++ TCPServer/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace TCPServer
+{
+    class Program
+    {
+        private static int _enteranceCount;
+
+        private static ConcurrentDictionary<string, TcpClient> _clients = new ConcurrentDictionary<string, TcpClient>();
+
+        static void Main(string[] args)
+        {
+            var server = new TcpListener(new IPEndPoint(IPAddress.Any, 50));
+            server.Start();
+            Console.WriteLine("Server started");
+
+            Listen();
+
+            while (true)
+            {
+                Console.WriteLine(1);
+                string clientName = "";
+
+                var client = server.AcceptTcpClient();
+                Console.WriteLine(2);
+                var stream = client.GetStream();
+                var reader = new StreamReader(stream);
+                if (stream.DataAvailable)
+                {
+                    clientName = reader.ReadLine();
+
+                    Console.WriteLine(clientName + " Entered");
+
+                    _clients.TryAdd(clientName, client);
+
+                    SendMessageToAll(clientName);
+                    Console.WriteLine($"Client count {++_enteranceCount}");
+                }
+            }
+        }
+
+        private async static void Listen()
+        {
+            await Task.Run(() =>
+            {
+                var flag = true;
+                string rKey = "";
+                TcpClient rValue;
+
+                while (true)
+                {
+                    while (flag)
+                    {
+                        foreach (var item in _clients)
+                        {
+                            var stream = item.Value.GetStream();
+                            var reader = new StreamReader(stream);
+                            if (stream.DataAvailable)
+                            {
+                                var msg = reader.ReadLine();
+
+                                if (msg == "close")
+                                {
+                                    msg = item.Key + " left the chat";
+                                    rKey = item.Key;
+                                    rValue = item.Value;
+                                    SendMessageToAll(msg);
+                                    Console.WriteLine(msg);
+                                    item.Value.Close();
+                                    flag = false;
+                                    break;
+                                }
+
+                                SendMessageToAll($"{item.Key}: {msg}");
+                                Console.WriteLine($"{item.Key}: {msg}");
+                            }
+                        }
+                    }
+                    flag = _clients.TryRemove(rKey, out rValue);
+                }
+            });
+        }
+
+        private static void SendMessageToAll(string message)
+        {
+            foreach (var item in _clients)
+            {
+                var stream = item.Value.GetStream();
+                var writer = new StreamWriter(stream);
+
+                writer.WriteLine(message);
+                writer.Flush();
+            }
+        }
+    }
+}
---
+++ Tcp_Clien2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Tcp_Client
+{
+    class Program
+    {
+        private TcpClient _client;
+        private StreamReader _reader;
+        private StreamWriter _writer;
+
+        private async Task Listen()
+        {
+            await Task.Run(() =>
+            {
+                while (true)
+                {
+                    if (_client.Connected && _client.GetStream().DataAvailable)
+                    {
+                        var text = _reader.ReadLine();
+                        Console.WriteLine(text);
+                    }
+                }
+            });
+        }
+
+        private void StartClient()
+        {
+            Random random = new Random();
+
+            _client = new TcpClient();
+            _client.Connect("127.0.0.1", 50);
+            var stream = _client.GetStream();
+            _reader = new StreamReader(stream);
+            _writer = new StreamWriter(stream);
+
+            var name = "Alex" + random.Next(1000).ToString();
+            _writer.WriteLine(name);
+            _writer.Flush();
+
+
+            Listen();
+
+            while (true)
+            {
+                var str = Console.ReadLine();
+                _writer.WriteLine(str);
+                _writer.Flush();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            new Program().StartClient();
+        }
+    }
+}
